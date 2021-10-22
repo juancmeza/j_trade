e1c975cc5b9032b10c8cec091cabf015e11dd59d
Juan Carlos Meza <juancmezat@gmail.com>
2021-10-22 02:00:02 -0700

    User model, login/sessions

--- db/migrate/20211022074222_create_users.rb
+++ db/migrate/20211022074222_create_users.rb
@@ -2,8 +2,9 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.decimal :portfolio
-      t.datetime :start
+      t.string :password_digest
+      t.decimal :balance
+      t.datetime :opendate
 
       t.timestamps
     end

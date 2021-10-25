5f9750d571af36b235243f0859c7b0993c82c507
Juan Carlos Meza <juancmezat@gmail.com>
2021-10-24 23:58:21 -0700

    Coins correction/setup

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   post '/login', to: 'auth#create'
   get '/persist', to: 'auth#show'
 
+  get 'users/coins/:id', :to => 'users#coins'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
